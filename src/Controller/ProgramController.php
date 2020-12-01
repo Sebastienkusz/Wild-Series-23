df0bafeaf7c8d02c1e324fdc5642b2d205fbf4e4
Sebastien kusz <kusz.sebastien@gmail.com>
2020-12-01 13:55:37 +0100

    Advanced routing

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -8,10 +8,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/programs", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
@@ -19,4 +22,15 @@ class ProgramController extends AbstractController
             'website' => 'Wild Séries',
         ]);
     }
+
+    /**
+     * @Route("/show/{page}", methods={"GET"}, requirements={"page"="\d+"}, name="show")
+     */
+    public function show(int $page): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'page' => $page,
+        ]);
+    }
+
 }
